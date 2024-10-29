bb292d87dd44e518c835b8ab5d82dcae5cb47c10
Dharmesh Patel <dspatel44@gmail.com>
2024-10-29 10:17:32 +0530

    Remove unwanted files.

--- tests/e2e/bootstrap/bootstrap.sh
+++ tests/e2e/bootstrap/bootstrap.sh
@@ -18,6 +18,7 @@ echo "Importing sample products"
 wp plugin install wordpress-importer --activate
 wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
 
+### TODO: Setup Stripe with Webhook, once we have a support for bootstraping using JS files
 echo "Configuring WooCommerce Gateway Stripe"
 echo " - Updating WooCommerce Gateway Stripe settings"
 wp option set woocommerce_stripe_settings --format=json "{\"enabled\":\"yes\",\"title\":\"Credit Card (Stripe)\",\"description\":\"Pay with your credit card via Stripe.\",\"api_credentials\":\"\",\"testmode\":\"yes\",\"test_publishable_key\":\"${STRIPE_PUB_KEY}\",\"test_secret_key\":\"${STRIPE_SECRET_KEY}\",\"publishable_key\":\"\",\"secret_key\":\"\",\"webhook\":\"\",\"test_webhook_secret\":\"\",\"webhook_secret\":\"\",\"inline_cc_form\":\"no\",\"statement_descriptor\":\"\",\"short_statement_descriptor\":\"\",\"capture\":\"yes\",\"payment_request\":\"yes\",\"payment_request_button_type\":\"buy\",\"payment_request_button_theme\":\"dark\",\"payment_request_button_locations\":[\"product\",\"cart\",\"checkout\"],\"payment_request_button_size\":\"default\",\"saved_cards\":\"yes\",\"logging\":\"no\",\"upe_checkout_experience_enabled\":\"yes\"}"
