fdfad845b6082f9cd202b33cf91cac13dc33d408
Wesley Rosa <wesleyjrosa@gmail.com>
2024-12-26 09:48:33 -0300

    Update includes/payment-methods/class-wc-stripe-payment-request.php

--- includes/payment-methods/class-wc-stripe-payment-request.php
+++ includes/payment-methods/class-wc-stripe-payment-request.php
@@ -534,7 +534,7 @@ class WC_Stripe_Payment_Request {
 			
 			$spaceless_postcode = preg_replace( '/\s+/', '', $postcode );
 			
-			if ( strlen($spaceless_postcode) < 5) { 
+			if ( strlen( $spaceless_postcode ) < 5) { 
 				// Always reintroduce the space so that Shipping Zones regex like 'N1 *' work to match N1 postcodes like N1 1AA, but don't match N10 postcodes like N10 1AA
 				return $spaceless_postcode . " ***"; 
 			} else {
